cfb2743e049c3565bcf92096554da0dfe794a87e
Do Duc Minh <22010176@st.phenikaa-uni.edu.vn>
2025-06-29 12:26:18 +0700

    ed

--- DatabaseModels/AppDbContext.cs
+++ DatabaseModels/AppDbContext.cs
@@ -9,9 +9,25 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
   public string ConnectionString { get; } = options.Extensions.OfType<RelationalOptionsExtension>().FirstOrDefault()?.ConnectionString!;
 
   public DbSet<NguoiDung> NguoiDung { get; set; }
+  public DbSet<LopHoc> LopHoc { get; set; }
+  public DbSet<LopHoc_NguoiDung> LopHoc_NguoiDung { get; set; }
 
   protected override void OnModelCreating(ModelBuilder modelBuilder)
   {
     base.OnModelCreating(modelBuilder);
+
+    var nguoiDung = modelBuilder.Entity<NguoiDung>();
+    var lopHoc = modelBuilder.Entity<LopHoc>();
+    var lopHoc_NguoiDung = modelBuilder.Entity<LopHoc_NguoiDung>();
+
+    nguoiDung.HasIndex(t => t.Email).IsUnique(true);
+    nguoiDung.HasIndex(t => t.SoDienThoai).IsUnique(true);
+    nguoiDung.Property(t => t.ThoiGianTao).HasDefaultValue("CURRENT_TIMESTAMP");
+
+    lopHoc_NguoiDung.HasIndex(t => new { t.IdLopHoc, t.IdNguoiDung }).IsUnique(true);
+    lopHoc_NguoiDung.Property(t => t.ThoiGianYeuCau).HasDefaultValue("CURRENT_TIMESTAMP");
+
+    lopHoc.HasIndex(t => t.MaLop).IsUnique(true);
+    lopHoc.Property(t => t.ThoiGianTao).HasDefaultValue("CURRENT_TIMESTAMP");
   }
 }
--- DatabaseModels/Models/NguoiDung.cs
+++ DatabaseModels/Models/NguoiDung.cs
@@ -23,5 +23,40 @@ public class NguoiDung
   public DateTime? ThoiGianTao { get; set; }
   public string? HinhAnh { get; set; }
   public LoaiNguoiDung? LoaiNguoiDung { get; set; }
+
+  public List<LopHoc_NguoiDung>? LopHoc_NguoiDung { get; set; }
+  public List<LopHoc>? LopHoc { get; set; }
 }
 
+public class LopHoc
+{
+  [Key]
+  public int Id { get; set; }
+  public string? MaLop { get; set; }
+  public string? TenLop { get; set; }
+  public string? MoTa { get; set; }
+  public DateTime? ThoiGianTao { get; set; }
+
+  public int IdGiangVien { get; set; }
+  public NguoiDung? NguoiTao { get; set; }
+
+  public List<LopHoc_NguoiDung>? LopHoc_NguoiDung { get; set; }
+}
+
+public enum TrangThai_LH_ND
+{
+  DANG_CHO, DA_DUYET, DA_HUY,
+}
+public class LopHoc_NguoiDung
+{
+  [Key]
+  public int Id { get; set; }
+  public DateTime? ThoiGianYeuCau { get; set; }
+  public TrangThai_LH_ND TrangThai { get; set; } = TrangThai_LH_ND.DANG_CHO;
+
+  public int IdLopHoc { get; set; }
+  public LopHoc? LopHoc { get; set; }
+
+  public int IdNguoiDung { get; set; }
+  public NguoiDung? NguoiDung { get; set; }
+}
